54f02bdf50e262bac55f7c34e9075a44779773da
karolv <karol.victorialima@gmail.com>
2026-06-14 17:39:19 -0300

    cli: integra __main__ ao conversor

--- src/convert/__main__.py
+++ src/convert/__main__.py
@@ -1,33 +1,35 @@
-"""
-Este arquivo é o ponto de entrada principal do pacote `python_pdm_template`.
-
-Função principal:
-- Define a função `main`, que é executada quando o pacote é chamado diretamente 
-  pela linha de comando.
-
-Como construir e usar:
-1. Certifique-se de que o projeto está configurado corretamente com o PDM.
-2. Instale seu pacote no ambiente virtual usando:
-   ```bash
-    python -m pdm install
-    ```
-3. Execute o comando abaixo para rodar o pacote diretamente:
-   ```bash
-   python -m pdm run python src/NOME_DO_PROJETO/__main__.py
-   ```
-"""
-
-from convert.utils import somar
+from convert.core.conversor import Conversor
+import sys
 
 
 def main():
-    """Função principal que exibe uma mensagem de boas-vindas."""
-    print("Hello, Python PDM Template!")
-    print()
-    print("A soma de 5+2 = ", somar(5, 2))
-    print()
+    """Ponto de entrada da aplicação."""
+
+    if len(sys.argv) != 4:
+        print(
+            "Uso: python -m convert "
+            "<arquivo> <formato> <saida>"
+        )
+        return
+
+    arquivo = sys.argv[1]
+    formato = sys.argv[2]
+    saida = sys.argv[3]
+
+    try:
+        conversor = Conversor()
+
+        resultado = conversor.converter(
+            arquivo,
+            formato,
+            saida
+        )
+
+        print(resultado)
+
+    except Exception as erro:
+        print(f"Erro: {erro}")
 
 
-# Verifica se o arquivo está sendo executado diretamente
 if __name__ == "__main__":
     main()
